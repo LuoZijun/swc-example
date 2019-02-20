8b91b7c53247ae3b3a5ff1283bbc4f181986720c
luozijun <luozijun.assistant@gmail.com>
2019-02-20 18:12:14 +0800

    Update src/prelude.js

--- src/prelude.js
+++ src/prelude.js
@@ -1,6 +1,6 @@
 if ( typeof global === 'undefined' ) {
     if ( typeof window === 'undefined' ) {
-        throw new Error("Unknow Process Platform (Only Support browser and node platform)!");
+        throw new Error("Only Support browser and node platform!");
     }
 
     window.global = window;
